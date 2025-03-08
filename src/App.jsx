3f4ed30bc701db1de606b9a8375e36dd93c28dcd
Alessandro Gramcko <107635555+Agramcko@users.noreply.github.com>
2025-03-08 17:16:35 -0400

    Ajuste y agregue el diseño del home

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,14 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
-import { Experiencias, Home } from './Home'
+import './index.css'
+import { Home } from './Home'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
 
-    <h1>Holaaa</h1>
     <Home/>
 
     </>
