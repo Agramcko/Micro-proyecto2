5c4a0a4a2cb69e150c00b6b8c72c0284877eddeb
Alessandro Gramcko <107635555+Agramcko@users.noreply.github.com>
2025-03-09 23:54:52 -0400

    agregue las credenciales de la base de datos

---
+++ credenciales.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBKO-OzvAaP_6RwUU6uh_9Tfc7kkgjtfV8",
+  authDomain: "avilatours-decde.firebaseapp.com",
+  projectId: "avilatours-decde",
+  storageBucket: "avilatours-decde.firebasestorage.app",
+  messagingSenderId: "352711222391",
+  appId: "1:352711222391:web:5f3525f00b8f3aa852d272"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
